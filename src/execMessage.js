b7fc74fd805bb37bc3f97ec05e740c80471d32f6
Sanjeev Gopinath <sanjeevgopinath@live.com>
2018-12-19 14:33:18 +0530

    Live Commit

--- src/execMessage.js
+++ src/execMessage.js
@@ -17,10 +17,7 @@ class execMessage extends Component {
                     <hr />
                     <div class="exec-message-content">
                         <p>Hello { localStorage.getItem("firstname") },</p>
-                        <p>Welcome to the FY19 India SE Symposium!</p>
-                        <p class="quote"><strong>“EVERYBODY GETS THE CULTURE THEY DESERVE”</strong></p>
-                        <p>And WE at the India SE team have managed to build our own culture by making SE symposium as an annual phenomenon. This year’s theme is our transformation from SC to SE – Sales Consulting to Solution Engineering, much needed transition for our journey towards India Cloud DC. Join us to hear from India SMT, To learn from Cloud experts, To build, extend and nurture your network, To share and get enlighten from each other’s experiences, most importantly to celebrate our success and to have fun.</p>
-                        <p>See you all at The Pink City!</p>
+                        <p>ARE YOU READY FOR BALI?</p>
                         <p>Regards,</p>
                         <p>Vaibhav Gawde</p>
                     </div>

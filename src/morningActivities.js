1e8150bd0ac4cfd180382dde14a2244a8c52b780
punix <frenchyrules@gmail.com>
2019-11-25 15:15:51 +0530

    morning activity update

--- src/morningActivities.js
+++ src/morningActivities.js
@@ -4,6 +4,14 @@ import Img  from 'react-image';
 import {config} from './config.js';
 
 import './cityTour.css'
+
+/*
+prekhade - 25-Nov-2019
+Morning Activity 1 - Exercise
+Morning Activity 2 - Heritage City Tour
+Morning Activity 3 - Golden Temple Visit
+*/
+
 class morningActivities extends Component {
 
     constructor(props) {
@@ -36,17 +44,17 @@ class morningActivities extends Component {
         .then(response => {
             console.log(response);
             for(var i=0; i < response.length; i++) {
-                if(response[i].eventName === "Nature Walk") {
+                if(response[i].eventName === "MorningActivity_1") {
                     this.setState({hideButton1: true});
                     this.setState({registeredContent: response[i].eventName});
                     break;
                 }
-                if(response[i].eventName === "Cycling") {
+                if(response[i].eventName === "MorningActivity_2") {
                     this.setState({hideButton1: true});
                     this.setState({registeredContent: response[i].eventName});
                     break;
                 }
-                if(response[i].eventName === "Yoga/Gym") {
+                if(response[i].eventName === "MorningActivity_3") {
                     this.setState({hideButton1: true});
                     this.setState({registeredContent: response[i].eventName});
                     break;
@@ -64,13 +72,13 @@ class morningActivities extends Component {
         console.log(val);
         var payload = '';
         if(val === 1) {
-            payload = 'Nature Walk';
+            payload = 'MorningActivity_1';
         }
         if(val === 2) {
-            payload = 'Cycling';
+            payload = 'MorningActivity_2';
         }
         if(val === 3) {
-            payload = 'Yoga/Gym';
+            payload = 'MorningActivity_3';
         }
         var payloadJSON = JSON.stringify({
             "eventName": payload,
@@ -100,7 +108,7 @@ class morningActivities extends Component {
         return(
             <div class="container">
                 <h3>Morning Activities</h3>
-                <p class="caption">Registration for Day 3</p>
+                <p class="caption">Registration for Day</p>
                 
                 <nav aria-label="Page navigation example">
                     <ul class="pagination">
@@ -111,28 +119,24 @@ class morningActivities extends Component {
                 </nav>
                 
                 { this.state.tourSelectedId === 1 && <div>
-                                <h5>Nature Walk: Guided tour to Smriti Van Jaipur</h5>
-                                <Img class="card-img-top tour_pic" src="https://aivloqpzen.cloudimg.io/width/400/n/http://sesymposium.tech/nature.jpg" alt="Card image cap" />
-                                <p>Location: Smirit Van is around 4 kms from Hotel Lalit. </p>
-                                <p>Reaching there: Coach is arranged for transfer from Hotel. Coach leaves sharp at 6 AM. It takes around 10--15 minutes to reach there. It can take 45 minutes to 1 hour to explore and cover the network of fine trails around the little hill & on ridge etc.</p>
-                                <p>About Location: Smriti Van Jaipur is a Bio-Diversity forest. During highly destructive flood of 1981 this area was inundated and deep ravines were formed. The area was taken upto plant trees in memory of the near and dear ones. </p>
-                                <p>You can enjoy Aloe Vera, Turmeric, Amla and other organic juices outside the Smriti Van.</p>
+                                <h3>Exercise</h3>
+                                <Img class="card-img-top tour_pic" src="MorningExercises.jpg" alt="Card image cap"/>
+                                
                                 { !this.state.hideButton1 && <button type="button" class="btn btn-success post-button" onClick={this.handleRegistration.bind(this, 1)}>Register</button> }
                                 </div> }
                 
                 
-                { this.state.tourSelectedId === 2 && <div><h5>Cycling: Cycle Track around Jawahar Circle</h5>
-                                <Img class="card-img-top tour_pic" src="https://aivloqpzen.cloudimg.io/width/400/n/http://sesymposium.tech/cycling.png" alt="Card image cap" />
-                                <p>Location: Jawahar Circle is around 600 meters from Hotel Lalit.</p>
-                                <p>Reaching there: A 5 mintues walk from Hotel will take up to Jawahar circle. We will pick up cycles there and ride around the Jawahar circle for 45 mins – 1 hour.</p>
-                                <p>Jawahar circle has an exclusive cycling track which runs for 1.5 km. </p>
-                                <p>On way back, around 100 meters before the Hotel, you can enjoy freshly prepared Hot Tea with matti</p>
+                { this.state.tourSelectedId === 2 && <div>
+                                <h3>Heritage City Tour</h3>
+                                <Img class="card-img-top tour_pic" src="city_tour.jpg" alt="Card image cap" />
+                                
                                 { !this.state.hideButton1 && <button type="button" class="btn btn-success post-button" onClick={this.handleRegistration.bind(this, 2)}>Register</button> } 
                                 </div> }
                 
                 
-                { this.state.tourSelectedId === 3 && <div><h5>Yoga and Gymnasium</h5>
-                                <p>Follow the trained and experienced Yoga instructor OR hit the Hotel Gymanisum.</p>
+                { this.state.tourSelectedId === 3 && <div>
+                                <h3>Golden Temple Visit</h3>
+                                <Img class="card-img-top tour_pic" src="golden_temple.jpg" alt="Card image cap" />
                                 { !this.state.hideButton1 && <button type="button" class="btn btn-success post-button" onClick={this.handleRegistration.bind(this, 3)}>Register</button> }
                                 </div> }
                 { <span class="tour-message">You have registered for { registeredContent }<br /></span>}
